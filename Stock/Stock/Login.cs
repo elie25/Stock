cab31af9069657e512046a8c62fb36182f94719a
Elie <elie.009@hotmail.com>
2020-08-30 08:56:07 +0300

    Added Stock Project Source Code

---
+++ Stock/Stock/Login.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Stock
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_MouseClick(object sender, MouseEventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Clear();
+            textBox1.Focus();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //TO-DO : check login username and password;
+            SqlConnection conn = new SqlConnection("Data Source=.;Initial Catalog=Stock;Integrated Security=True");
+            SqlDataAdapter sda = new SqlDataAdapter(@"SELECT * FROM[Stock].[dbo].[Login] WHERE Username = '" + textBox1.Text +"' and Password = '" + textBox2.Text + "'", conn);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            if(dt.Rows.Count == 1)
+            {
+                this.Hide();
+                StockMain main = new StockMain();
+                main.Show();
+            }
+            else
+            {
+                MessageBox.Show("Invalid Username & Password ..!", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
